fb85cfda7511a857eedded6039f25c5884255255
Hsuan Hao <hao66bmbm@gmail.com>
2021-05-05 00:15:46 +0800

    reset route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import *
 import mysql.connector
 from custom_models import UseData
+
 from flask_cors import CORS
 
 
@@ -9,10 +10,8 @@ app=Flask(
     static_folder="static",
     static_url_path="/")
 CORS(app)
-
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
-
 
 
 
@@ -23,14 +22,7 @@ def index():
     return render_template("index.html")
 @app.route("/attraction/<id>")
 def attraction(id):
-    IdCount=UseData.CheakIdCount()
-    id=int(id)
-    if id<=int(IdCount) and id>=1:
-        data=UseData.LoadDataToId(id)
-        return Response(json.dumps({"data":data}, sort_keys=False),mimetype='application/json')
-    else:
-        abort(400)
-    #     return render_template("attraction.html")
+    return render_template("attraction.html")
 @app.route("/booking")
 def booking():
     return render_template("booking.html")
@@ -44,23 +36,13 @@ def getbaseapi():
     WebKeyword=request.args.get("keyword",None)   
     print("WebPage:",WebPage,"WebKeyword:",WebKeyword)
     data=UseData.LoadDataToDB(WebPage,WebKeyword)
-#   return jsonify({"nextpage":WebPage,"data":data})
-    # if (len(data)==12):
-    #     return Response(json.dumps({"nextpage":int(WebPage)+1,"data":data}, sort_keys=False),mimetype='application/json')
-    # if (len(data)<12) and (len(data)>=1):
-    #     return Response(json.dumps({"nextpage":None,"data":data}, sort_keys=False),mimetype='application/json')
-    
+
     data=data[12*int(WebPage):12*(int(WebPage)+1)]
     if (len(data)>=12):
         if (len(data)==12):
             return Response(json.dumps({"nextpage":int(WebPage)+1,"data":data}, sort_keys=False),mimetype='application/json')
     if (len(data)<12) and (len(data)>=1):
         return Response(json.dumps({"nextpage":None,"data":data}, sort_keys=False),mimetype='application/json')
-
-
-    
-
-
     else:
         abort(500)
 
@@ -73,7 +55,18 @@ def page_400(error):
 def page_500(error):
     return Response(json.dumps({"error":True,"message": "自訂的錯誤訊息"}, sort_keys=False),mimetype='application/json'),500
 
+@app.route("/api/attraction/<attractionId>")
+def attractionId(attractionId):
+    IdCount=UseData.CheakIdCount()
+    attractionId=int(attractionId)
+    if attractionId<=int(IdCount) and attractionId>=1:
+        data=UseData.LoadDataToId(attractionId)
+        return Response(json.dumps({"data":data}, sort_keys=False),mimetype='application/json')
+    else:
+        abort(400)
+
+
 
 app.run(host="0.0.0.0", port=3000)
-
 # app.run(port=3000)
+
