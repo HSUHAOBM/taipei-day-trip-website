8bba5c072734433003c9fa6d8efb56c36f1b6a3b
Hsuan Hao <hao66bmbm@gmail.com>
2021-05-08 16:25:47 +0800

    add button and modify css.

--- app.py
+++ app.py
@@ -73,6 +73,6 @@ def attractionId(attractionId):
 
 
 # app.run(host="0.0.0.0", port=3000, debug=True)
-# app.run(host="0.0.0.0", port=3000)
-app.run(port=3000, debug=True)
+app.run(host="0.0.0.0", port=3000)
+# app.run(port=3000, debug=True)
 
